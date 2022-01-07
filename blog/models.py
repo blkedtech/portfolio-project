11bebd8aa8b212734081edafc103ec27eba4f2ab
blkedtech <blkedtech@gmail.com>
2022-01-06 17:24:56 -0800

    Blog app up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# Create a Blog models
+class Blog(models.Model):
+    # title
+    title = models.CharField(max_length=255)
+    # pub_date
+    pub_date = models.DateTimeField()
+    # body
+    body = models.TextField()
+    # image
+    image = models.ImageField(upload_to='images/')
+
+# Add the Blog to the "settings": ('blog.apps.BlogConfig',)
+
+# Create a migration: (> python manage.py makemigration)
+
+# Migrate: (> python manage.py migrate)
+
+# Add to the "admin": (from .models import Blog) (admin.site.register(Blog)
