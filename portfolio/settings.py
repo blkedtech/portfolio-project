078e6aad48981a03841b8cfe543bdfbbf538c801
blkedtech <blkedtech@gmail.com>
2022-01-09 06:48:58 -0800

    Changed password to default

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD':'Pa11word',
+        'PASSWORD':'django123',
         'HOST':'localhost',
         'PORT':'5432',
     }
