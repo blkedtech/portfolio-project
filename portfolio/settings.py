aff137556b6a17d17533313b6e5d85745bd60641
blkedtech <blkedtech@gmail.com>
2022-01-09 06:57:24 -0800

    Added local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,3 +137,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
+
+try:
+    from local_settings import *
+excpet ImportError:
+    pass
